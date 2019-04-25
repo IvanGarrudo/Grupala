b381bcb04aae1b731565702a441a7bbf3e769068
YujiroS <100346224@alumnos.uc3m.es>
2019-04-25 15:52:58 +0200

    Mis grupos

---
+++ src/pages/misgrupos/misgrupos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { MisgruposPage } from './misgrupos';
+
+@NgModule({
+  declarations: [
+    MisgruposPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(MisgruposPage),
+  ],
+})
+export class MisgruposPageModule {}

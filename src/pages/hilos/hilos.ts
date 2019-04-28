eab3263a557ad425d172dff7cf526ac23dd97f51
IvanGarrudo <ivangarrudod@gmail.com>
2019-04-28 19:23:35 +0200

    hilos bien con noticias, y que se puedan meter mensajes

--- src/pages/hilos/hilos.ts
+++ src/pages/hilos/hilos.ts
@@ -1,5 +1,9 @@
+import { mensajes } from './mensajes';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Noticia } from '../../models/noticia.model'
+import { NoticiaService } from '../../services/noticia.service';
+
 
 /**
  * Generated class for the HilosPage page.
@@ -15,7 +19,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HilosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public listam:Array<mensajes> = [new mensajes("../assets/imgs/user3.png","Considero que no tienes razón 5º es mejor"), new mensajes("../assets/imgs/user2.png","3.5 es lo unico de D&D que se debería considerar rol"), new mensajes("../assets/imgs/user1.png","Creo que todas las versiones tienen su público, aunque me inclino más por la primera versión, qué opninan ustedes?"),];
+  constructor(public navCtrl: NavController, public navParams: NavParams, private Nueva :NoticiaService) {
+  }
+
+  add(nuevo:mensajes){
+    this.listam.push(new mensajes("../assets/imgs/user1.png", nuevo.mensaje));
+    this.Nueva.add(new Noticia("chatboxes","catwoman a escrito \""+nuevo.mensaje+"\" en el foro de Manuales"));
   }
 
   ionViewDidLoad() {
---
+++ src/pages/hilos/mensajes.ts
@@ -0,0 +1,9 @@
+export class mensajes{
+    public icono:string;
+    public mensaje:string;
+
+    constructor(icono:string, mensaje:string){
+        this.icono=icono;
+        this.mensaje=mensaje;
+    }
+}

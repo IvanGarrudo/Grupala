90ae8f66de0cf97c6d5130618eb200d4754b208e
devildgl <devildgl98@gmail.com>
2019-04-28 23:01:33 +0200

    nombre guardado

--- src/pages/mis-eventos/mis-eventos.ts
+++ src/pages/mis-eventos/mis-eventos.ts
@@ -1,3 +1,4 @@
+import { Event } from './../../models/event.model';
 import { ContactService } from './../../services/contact.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -19,7 +20,6 @@ import { NuevoEventoPage } from '../nuevo-evento/nuevo-evento';
 export class MisEventosPage {
   events: Event[] = []
   public searchTerm: string = "";
-  user="";
   constructor(public navCtrl: NavController, public navParams: NavParams, private EventService: EventService,private ContactService:ContactService) {
   }
 
@@ -27,8 +27,8 @@ export class MisEventosPage {
     this.events = this.EventService.getGroup();
   }
   unir(){
-    alert(this.ContactService.getu())
-    this.user="-"+this.ContactService.getu();
+    this.EventService.sss();
+    this.events = this.EventService.getGroup();
   }
   onLoadEventPage(){
     this.navCtrl.push(NuevoEventoPage);
